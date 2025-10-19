9b5af4718ab4960dc72d641f3ab8e070d1c509bd
Quinn Lue <luequinn@gmail.com>
2025-10-18 20:14:03 -0600

    Added kv caching for inference

--- modules/transformer.py
+++ modules/transformer.py
@@ -50,6 +50,56 @@ class Transformer(Module):
         self.ln1 = self.layer_norm(shape=d_model, axis=-1, module_dict=module_dict, name="ln1")
         self.ln2 = self.layer_norm(shape=d_model, axis=-1, module_dict=module_dict, name="ln2")
 
+
+    def cached_attend(self, x: Tensor, k_cache: Tensor, v_cache: Tensor):
+        # kv_cache is of shape (batch_size, max_seq_len, d_model)
+        # x is of shape (batch_size, seq_len, d_model)
+        B, T, _ = x.shape
+        if B != 1:
+            raise ValueError(f"Inference mode only supports batch size 1")
+
+        x = x[:, -1, :]
+        qkv = self.qkv(x)
+        q = qkv[:, :, :self.d_model]
+        k = qkv[:, :, self.d_model:self.d_model * 2]
+        v = qkv[:, :, self.d_model * 2:]
+
+        if self.lora:
+            q_lora_delta = self.scaling * (x @ self.q_lora_A.weight@ self.q_lora_B.weight)
+            k_lora_delta = self.scaling * (x @ self.k_lora_A.weight@ self.k_lora_B.weight)
+            v_lora_delta = self.scaling * (x @ self.v_lora_A.weight@ self.v_lora_B.weight)
+            q = q + q_lora_delta
+            k = k + k_lora_delta
+            v = v + v_lora_delta
+
+
+        k_cache[:, T-1, :] = k
+        v_cache[:, T-1, :] = v
+
+        q = q.reshape((B, 1, self.n_heads, self.d_head))
+        k_cached = k_cache[:, :T, :].reshape((B, T, self.n_heads, self.d_head))
+        v_cached = v_cache[:, :T, :].reshape((B, T, self.n_heads, self.d_head))
+
+        q = q.transpose((0, 2, 1, 3))
+        k_cached = k_cached.transpose((0, 2, 1, 3))
+        v_cached = v_cached.transpose((0, 2, 1, 3))
+        kt_cached = k_cached.transpose((0, 1, 3, 2))
+
+        atten_scores = q @ kt_cached * (1 / (self.d_head ** 0.5))
+
+        atten_probs = self.softmax(atten_scores, axis=3) # (B, n_heads, 1, T)
+
+        output = atten_probs @ v_cached
+
+        output = output.transpose((0, 2, 1, 3))
+        output = output.reshape((B, 1, -1))
+        if self.lora:
+            output = output + self.scaling * (output @ self.o_lora_A.weight@ self.o_lora_B.weight)
+        output = self.o(output)
+        return output
+
+
+
     
     def attend(self, x: Tensor):
         B, T, _ = x.shape
@@ -114,8 +164,16 @@ class Transformer(Module):
             x = self.proj_down(x)
         return x
 
-    def forward(self, x: Tensor):
-        x = x + self.attend(self.ln1(x))
+    def forward(
+        self, x: Tensor, 
+        k_cache: Tensor | None = None, 
+        v_cache: Tensor | None = None
+    ):
+        if k_cache is not None and v_cache is not None:
+            x = x + self.cached_attend(self.ln1(x), k_cache, v_cache)
+        else:
+            x = x + self.attend(self.ln1(x))
+
         x = self.ln2(x)
         mlp_out = self.mlp(x)
         x = x + mlp_out
